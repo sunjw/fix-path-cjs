ecfe4e3d15511e6922d3234d96c7e856a29b044d
Sun Junwen <sunjw8888@gmail.com>
2024-08-15 11:00:30 +0800

    Code format.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const ESMHelper = require('./esm-helper');
 const fixPathCjsWrapper = ESMHelper.requireESM('@sunjw8888/fix-path-cjs-wrapper');
 
 function fixPath() {
-  fixPathCjsWrapper.fixPathWrapper();
+    fixPathCjsWrapper.fixPathWrapper();
 }
 
 exports.fixPath = fixPath;
