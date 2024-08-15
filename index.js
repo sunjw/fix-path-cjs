a62d54234b0a648a838eddd8d0d8ce4c3a95f7c5
Sun Junwen <sunjw8888@gmail.com>
2024-08-15 10:45:51 +0800

    Init fix-path-cjs.

---
+++ index.js
@@ -0,0 +1,8 @@
+const ESMHelper = require('./esm-helper');
+const fixPathCjsWrapper = ESMHelper.requireESM('@sunjw8888/fix-path-cjs-wrapper');
+
+function fixPath() {
+  fixPathCjsWrapper.fixPathWrapper();
+}
+
+exports.fixPath = fixPath;
